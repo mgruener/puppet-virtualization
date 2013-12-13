dfdc00fcb34f5d9888b430456fb3a7656b7272d9
Michael Gruener <michael.gruener@chaosmoon.net>
2013-12-13 14:56:40 +0100

    - Separate public and private provider methods - add basic resource status checks

--- lib/puppet/provider/libvirt.rb
+++ lib/puppet/provider/libvirt.rb
@@ -10,18 +10,25 @@ Puppet::Type.type(:guest).provide(:libvirt) do
 
   defaultfor :virtual => ["kvm", "physical", "xenu"]
 
-  def virtinstall_version
-    @virtinstall_version ||= virtinstall("--version")
-  end
-
-  def virsh_version
-    @virsh_version ||= virsh("--version")
-  end
-
+  # Check if the domain exists.
   def exists?
+    exec
+    true
+  rescue Libvirt::RetrieveError => e
+    false # The vm with that name doesnt exist
   end
 
   def status
+    if exists?
+        return :installed
+    else
+      if resource[:ensure].to_s == "purged"
+        # TODO: do purged tests
+        return :purged
+      end
+      debug "Domain %s status: absent" % [resource[:name]]
+      return :absent
+    end
   end
 
   def create
@@ -32,4 +39,30 @@ Puppet::Type.type(:guest).provide(:libvirt) do
 
   def purge
   end
+
+private
+  def virtinstall_version
+    @virtinstall_version ||= virtinstall("--version")
+  end
+
+  def virsh_version
+    @virsh_version ||= virsh("--version")
+  end
+
+  def hypervisor
+    case resource[:virttype]
+      when :xen_fullyvirt, :xen_paravirt then "xen:///"
+      else "qemu:///system"
+    end
+  end
+
+  # Executes operation over guest
+  def exec
+    conn = Libvirt::open(hypervisor)
+    @guest = conn.lookup_domain_by_name(resource[:name])
+    ret = yield if block_given?
+    conn.close
+    return ret
+  end
+
 end
